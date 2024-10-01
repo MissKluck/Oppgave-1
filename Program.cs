8a4d1480c40f2824984293bcc22a343dbd13a285
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-01 14:27:15 +0200

    added int, double, string & char

--- Program.cs
+++ Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        int loop = 10;
+        Console.WriteLine(loop);
+        double dee = 12.5;
+        Console.WriteLine(dee + loop);
+        string tinkaton = "Tinkaton is the best!";
+        Console.WriteLine(tinkaton);
+        char b = 'y'; 
+
+        Console.WriteLine(b);
+
+        int[] myIntArray = {2,4,6,3,8,39,5,8};
+
     }
 }
