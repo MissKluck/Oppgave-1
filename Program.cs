929417212d9922d9e48c096fc47b346c1eb5dfbe
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-04 14:38:16 +0200

    finished all tasks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Oppgave_1;
+﻿//Oppgave 1
+namespace Oppgave_1;
 
 class Program
 {
@@ -6,23 +7,77 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         
-        int loop = 10;
-        Console.WriteLine(loop);
-        double dee = 12.5;
-        Console.WriteLine(dee + loop);
-        string tinkaton = "Tinkaton is the best!";
-        Console.WriteLine(tinkaton);
+        //Oppgave 2
+        int integer = 10;
+        Console.WriteLine(integer);
+        double dobbel = 12.5;
+        Console.WriteLine(dobbel + integer);
+        string text = "Hello there!";
+        Console.WriteLine(text);
         char b = 'y'; 
 
         Console.WriteLine(b);
 
         int[] myIntArray = {2,4,6,3,8,39,5,8};
-        Console.WriteLine(myIntArray);
 
-        for (int i = 0; i < myIntArray; i++)
+        for (int i = 0; i < myIntArray.Length; i++) 
+        /* 
+        Itererer fra indeks 0 til arrayets lengde
+        i = 0 --> det sier at indeksen, altså elementets posisjon i arrayet, begynner å telle fra null. 
+        Det betyr at selv om det første tallet er i arrayet skulle være 1, så er posisjonen i arrayet fortsatt 0
+        */
         {
             Console.WriteLine(myIntArray[i]);
         }
 
+        string [] myStringArray = {"Dette", "Er", "En", "String!"};
+        foreach (string s in myStringArray)
+        {
+            Console.WriteLine(s);
+        }
+
+        List<string> myStringList = new List<string>();
+        myStringList.Add("Pink");
+        myStringList.Add("Yellow");
+        myStringList.Add("Blue");
+        myStringList.Add("Turquoise");
+
+        foreach (string s in myStringList)
+        {
+            Console.WriteLine(s);
+        }
+
+        // Oppgave 3
+        // Opprett dictionary med int som nøkkel og string som verdi
+        Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+
+        // Legg til verdier til dictionarien
+        myDictionary.Add(1, "Obi-Wan Kenobi");
+        myDictionary.Add(2, "Anakin Skywalker");
+        myDictionary.Add(3, "Yoda");
+        myDictionary.Add(4, "Mace Windu");
+
+        // Bruk en foreach-løkke med var for å skrive ut verdiene i dictionary
+        foreach (var item in myDictionary)
+        {
+            Console.WriteLine($"Nøkkel: {item.Key}, Verdi: {item.Value}");
+        } 
+
+        int sum = MyIntMethod(5, 10);
+        Console.WriteLine($"Summen av 5 og 10 er: {sum}");
+
+        string navn = MyGreetingMethod("Kenobi");
+        Console.WriteLine(navn);
+
+    }
+
+     static public int MyIntMethod(int a, int b)
+    {
+        return a + b;
+    }
+
+    static public string MyGreetingMethod(string navn)
+    {
+        return $"Hello there! General {navn}!";
     }
 }
--- obj/Debug/net8.0/Oppgave 1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Oppgave 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oppgave 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d159fd46de0a18493e1432f07ad3f6e1fc3b05e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f2f71d945cdabd51ae9ed78791cf067c642243")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oppgave 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oppgave 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
